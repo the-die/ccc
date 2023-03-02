1e6c2b58ea2f4c2f9eae57b9b1890601fa27f811
the-die <xiulieshan@gmail.com>
2023-03-02 11:17:33 +0800

    Add *, / and ()

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+//
+// Tokenizer
+//
 
 typedef enum {
   TK_PUNCT, // Punctuators
@@ -167,8 +172,8 @@ static Token *tokenize(void) {
       continue;
     }
 
-    // Punctuator
-    if (*p == '+' || *p == '-') {
+    // Punctuators
+    if (ispunct(*p)) {
       cur = cur->next = new_token(TK_PUNCT, p, p + 1);
       p++;
       continue;
@@ -181,12 +186,234 @@ static Token *tokenize(void) {
   return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  int val;       // Used if kind == ND_NUM
+};
+
+static Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+static Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+static Node *expr(Token **rest, Token *tok);
+static Node *mul(Token **rest, Token *tok);
+static Node *primary(Token **rest, Token *tok);
+
+// A recursive descendent parser
+//
+// expr = mul ("+" mul | "-" mul)*
+// mul = primary ("*" primary | "/" primary)*
+// primary = "(" expr ")" | num
+//
+// expr: Expression
+// mul: Multiplication or division
+// primary: The most basic word in syntax
+
+// expr = mul ("+" mul | "-" mul)*
+static Node *expr(Token **rest, Token *tok) {
+  Node *node = mul(&tok, tok);
+
+  for (;;) {
+    if (equal(tok, "+")) {
+      node = new_binary(ND_ADD, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "-")) {
+      node = new_binary(ND_SUB, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
+  }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+static Node *mul(Token **rest, Token *tok) {
+  Node *node = primary(&tok, tok);
+
+  for (;;) {
+    if (equal(tok, "*")) {
+      node = new_binary(ND_MUL, node, primary(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "/")) {
+      node = new_binary(ND_DIV, node, primary(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
+  }
+}
+
+// primary = "(" expr ")" | num
+static Node *primary(Token **rest, Token *tok) {
+  if (equal(tok, "(")) {
+    Node *node = expr(&tok, tok->next);
+    *rest = skip(tok, ")");
+    return node;
+  }
+
+  if (tok->kind == TK_NUM) {
+    Node *node = new_num(tok->val);
+    *rest = tok->next;
+    return node;
+  }
+
+  error_tok(tok, "expected an expression");
+}
+
+//
+// Code generator
+//
+
+static int depth;
+
+static void push(void) {
+  // PUSH—Push Word, Doubleword, or Quadword Onto the Stack
+  // Decrements the stack pointer and then stores the source operand on the top of the stack.
+  printf("  push %%rax\n");
+  depth++;
+}
+
+static void pop(char *arg) {
+  // POP—Pop a Value From the Stack
+  // Loads the value from the top of the stack to the location specified with the destination
+  // operand (or explicit opcode) and then increments the stack pointer. The destination operand can
+  // be a general-purpose register, memory location, or segment register
+  printf("  pop %s\n", arg);
+  depth--;
+}
+
+static void gen_expr(Node *node) {
+  if (node->kind == ND_NUM) {
+    // T&T immediate operands are preceded by ‘$’; Intel immediate operands are
+    // undelimited (Intel ‘push 4’ is AT&T ‘pushl $4’). AT&T register operands are
+    // preceded by ‘%’; Intel register operands are undelimited.
+    //
+    // AT&T and Intel syntax use the opposite order for source and destination
+    // operands. Intel ‘add eax, 4’ is ‘addl $4, %eax’. The ‘source, dest’
+    // convention is maintained for compatibility with previous Unix assemblers.
+    //
+    // System V AMD64 ABI
+    // %rax: temporary register; with variable arguments passes information about
+    // the number of vector registers used; 1st return register
+
+    // long int strtol (const char* str, char** endptr, int base);
+    // Convert string to long integer
+    // Parses the C-string str interpreting its content as an integral number of the specified base,
+    // which is returned as a long int value. If endptr is not a null pointer, the function also
+    // sets the value of endptr to point to the first character after the number.
+    printf("  mov $%d, %%rax\n", node->val);
+    return;
+  }
+
+  gen_expr(node->rhs);
+  push();
+  gen_expr(node->lhs);
+  // %rdi: used to pass 1st argument to functions
+  pop("%rdi");
+  // now %rax is lhs value, %rdi is rhs value
+
+  switch (node->kind) {
+  case ND_ADD:
+    // ADD—Add
+    // Adds the destination operand (first operand) and the source operand (second operand) and
+    // then stores the result in the destination operand. The destination operand can be a
+    // register or a memory location; the source operand can be an immediate, a register, or a
+    // memory location. (However, two memory operands cannot be used in one instruction.) When an
+    // immediate value is used as an operand, it is sign-extended to the length of the destination
+    // operand format.
+    printf("  add %%rdi, %%rax\n");
+    return;
+  case ND_SUB:
+    // SUB—Subtract
+    // Subtracts the second operand (source operand) from the first operand (destination operand)
+    // and stores the result in the destination operand. The destination operand can be a register
+    // or a memory location; the source operand can be an immediate, register, or memory location.
+    // (However, two memory operands cannot be used in one instruction.) When an immediate value is
+    // used as an operand, it is sign-extended to the length of the destination operand format.
+    printf("  sub %%rdi, %%rax\n");
+    return;
+  case ND_MUL:
+    // IMUL—Signed Multiply
+    // Performs a signed multiplication of two operands. This instruction has three forms, depending
+    // on the number of operands.
+    printf("  imul %%rdi, %%rax\n");
+    return;
+  case ND_DIV:
+    // CWD/CDQ/CQO—Convert Word to Doubleword/Convert Doubleword to Quadword
+    // Doubles the size of the operand in register AX, EAX, or RAX (depending on the operand size)
+    // by means of sign extension and stores the result in registers DX:AX, EDX:EAX, or RDX:RAX,
+    // respectively. The CWD instruction copies the sign (bit 15) of the value in the AX register
+    // into every bit position in the DX register. The CDQ instruction copies the sign (bit 31) of
+    // the value in the EAX register into every bit position in the EDX register. The CQO
+    // instruction (available in 64-bit mode only) copies the sign (bit 63) of the value in the RAX
+    // register into every bit position in the RDX register.
+    printf("  cqo\n");
+    // IDIV—Signed Divide
+    // Divides the (signed) value in the AX, DX:AX, or EDX:EAX (dividend) by the source operand
+    // (divisor) and stores the result in the AX (AH:AL), DX:AX, or EDX:EAX registers. The source
+    // operand can be a general-purpose register or a memory location. The action of this
+    // instruction depends on the operand size (dividend/divisor).
+    //
+    // In 64-bit mode, the instruction’s default operation size is 32 bits. Use of the REX.R prefix
+    // permits access to additional registers (R8-R15). Use of the REX.W prefix promotes operation
+    // to 64 bits. In 64-bit mode when REX.W is applied, the instruction divides the signed value in
+    // RDX:RAX by the source operand. RAX contains a 64-bit quotient; RDX contains a 64-bit
+    // remainder.
+    printf("  idiv %%rdi\n");
+    return;
+  }
+
+  error("invalid expression");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
+  // Tokenize and parse.
   current_input = argv[1];
   Token *tok = tokenize();
+  Node *node = expr(&tok, tok);
+
+  if (tok->kind != TK_EOF)
+    error_tok(tok, "extra token");
 
   // https://sourceware.org/binutils/docs/as.html
   // https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html
@@ -213,53 +440,8 @@ int main(int argc, char **argv) {
   // definition overrides any other definitions.
   printf("main:\n");
 
-  // T&T immediate operands are preceded by ‘$’; Intel immediate operands are
-  // undelimited (Intel ‘push 4’ is AT&T ‘pushl $4’). AT&T register operands are
-  // preceded by ‘%’; Intel register operands are undelimited.
-  //
-  // AT&T and Intel syntax use the opposite order for source and destination
-  // operands. Intel ‘add eax, 4’ is ‘addl $4, %eax’. The ‘source, dest’
-  // convention is maintained for compatibility with previous Unix assemblers.
-  //
-  // System V AMD64 ABI
-  // %rax: temporary register; with variable arguments passes information about
-  // the number of vector registers used; 1st return register
-
-  // long int strtol (const char* str, char** endptr, int base);
-  // Convert string to long integer
-  // Parses the C-string str interpreting its content as an integral number of the specified base,
-  // which is returned as a long int value. If endptr is not a null pointer, the function also sets
-  // the value of endptr to point to the first character after the number.
-  //
-  // The first token must be a number
-  printf("  mov $%d, %%rax\n", get_number(tok));
-  tok = tok->next;
-
-  // ... followed by either `+ <number>` or `- <number>`.
-  while (tok->kind != TK_EOF) {
-    if (equal(tok, "+")) {
-      // ADD—Add
-      // Adds the destination operand (first operand) and the source operand (second operand) and
-      // then stores the result in the destination operand. The destination operand can be a
-      // register or a memory location; the source operand can be an immediate, a register, or a
-      // memory location. (However, two memory operands cannot be used in one instruction.) When an
-      // immediate value is used as an operand, it is sign-extended to the length of the destination
-      // operand format.
-      printf("  add $%d, %%rax\n", get_number(tok->next));
-      tok = tok->next->next;
-      continue;
-    }
-
-    tok = skip(tok, "-");
-    // SUB—Subtract
-    // Subtracts the second operand (source operand) from the first operand (destination operand)
-    // and stores the result in the destination operand. The destination operand can be a register
-    // or a memory location; the source operand can be an immediate, register, or memory location.
-    // (However, two memory operands cannot be used in one instruction.) When an immediate value is
-    // used as an operand, it is sign-extended to the length of the destination operand format.
-    printf("  sub $%d, %%rax\n", get_number(tok));
-    tok = tok->next;
-  }
+  // Traverse the AST to emit assembly.
+  gen_expr(node);
 
   // RET—Return From Procedure
   // Transfers program control to a return address located on the top of the
@@ -267,5 +449,7 @@ int main(int argc, char **argv) {
   // and the return is made to the instruction that follows the CALL
   // instruction.
   printf("  ret\n");
+
+  assert(depth == 0);
   return 0;
 }
